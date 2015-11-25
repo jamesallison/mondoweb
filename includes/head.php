9f0da4c565ecd4ef9abe5e6e23809ed7903334e6
James Allison <jamesallison@Jamess-MacBook-Pro.local>
2015-11-25 14:26:54 +0000

    Customised navbar
    Mondo colours

--- includes/head.php
+++ includes/head.php
@@ -1,2 +1,3 @@
 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-MfvZlkHCEqatNoGiOXveE8FIwMzZg4W85qfrfIFBfYc= sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
 <link href="/assets/css/style.css" rel="stylesheet">
+<link href="/assets/css/navbar.css" rel="stylesheet">
