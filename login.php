8a9d4bd71155c2a869b6d8ab804cd17904c67bc5
Kieran McHugh <kieranmch@icloud.com>
2015-11-27 12:54:16 +0000

    Styled login screen

--- login.php
+++ login.php
@@ -5,35 +5,29 @@
 		<link href="/assets/css/login.css" rel="stylesheet">
 	</head>
 	<body>
-		<div class="container">
-			<form class="form-signin" action="/scripts/login.php" method="post">
-				<div class="form-signin-heading">
-					<div class="row">
-						<div class="col-xs-2">
-							<br>
-							<img src="/assets/img/mondo.png" width="200%">
-						</div>
-						<div class="col-xs-10">
-							<h2 class="text-center">Login to Mondo</h2>
-						</div>
-					</div>
-					<?php
-						if(isset($_GET['expired'])) {
-							echo '<div class="alert alert-warning">Your access token expired, please login again.</div>';
-						}
-					?>
-				</div>
-				<label for="inputEmail" class="sr-only">Email address</label>
-				<input type="email" id="inputEmail" name="username" class="form-control" placeholder="Email address" required autofocus>
-				<label for="inputPassword" class="sr-only">Password</label>
-				<input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required>
-				<!--<div class="checkbox">
-					<label>
-						<input type="checkbox" value="remember-me"> Remember me
-					</label>
-				</div>-->
-				<button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-			</form>
-		</div>
+        <div class="wrapper">
+            <div class="container">
+                <form class="form-signin" action="/scripts/login.php" method="post">
+                    <div class="form-signin-heading">
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <img id="logo" src="/assets/img/mondo.png">
+                                <h2 class="text-center">Log In to Mondo</h2>
+                            </div>
+                        </div>
+                        <?php
+                        if(isset($_GET['expired'])) {
+                            echo '<div class="alert alert-warning">Your access token expired, please login again.</div>';
+                        }
+                        ?>
+                    </div>
+                    <label for="inputEmail" class="sr-only">Email address</label>
+                    <input type="email" id="inputEmail" name="username" class="form-control" placeholder="Email address" required autofocus>
+                    <label for="inputPassword" class="sr-only">Password</label>
+                    <input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required>
+                    <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+                </form>
+            </div>
+        </div>
 	</body>
 </html>
